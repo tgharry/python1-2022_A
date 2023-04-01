7ea2f54badfc7084c833391c261ec14ecf97679b
XXXX <mikiplxd1@gmail.com>
2023-04-01 11:21:41 +0200

    test 3

--- src/algo/z4/trains/a.py
+++ src/algo/z4/trains/a.py
@@ -1,4 +1,5 @@
 from gen_train_data import generate_data
+from collections import Counter
 """
 zadanie1:
  Mając dany "rozkład jazdy (z pliku gen_train_data.py), wyświetlić miasto z którego jest najwięcej
@@ -11,7 +12,17 @@ def get_city_most_connections(train_data: list[tuple[int,int]]) -> int:
     maxs = 0
     for tupla in train_data:
         a, b = tupla
-        z = int(b) - int(a)
+        list.append(b)
+    couns = Counter(list)
+    print(list)
+    print(couns.most_common(3))
+
+
+
+get_city_most_connections(generate_data(10))
+
+"""""
+z = int(b) - int(a)
         if(z < 0):
             z = z*-1
             if(maxs < z):
@@ -26,7 +37,4 @@ def get_city_most_connections(train_data: list[tuple[int,int]]) -> int:
             else:
                 list.append(f'{tupla} : {z}')
     print(f'{list}\n------------\n{finale}')
-
-
-
-get_city_most_connections(generate_data(50))
+"""""
