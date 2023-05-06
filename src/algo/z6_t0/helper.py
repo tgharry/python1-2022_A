0d7f65d385122607d1b9dc5e792fb4c351aff853
tgharry <117014206+tgharry@users.noreply.github.com>
2023-05-06 15:11:35 +0200

    Update helper.py

--- src/algo/z6_t0/helper.py
+++ src/algo/z6_t0/helper.py
@@ -2,5 +2,13 @@ from model import Fraction
 
 
 def add(f1: Fraction, f2: Fraction) -> Fraction:
-    # write me
-    return f1  # obviously false
+    a_up, b_down = (f1.numer, f1.denom)
+    c_up, d_down = (f2.numer, f2.denom)
+    if(f1.denom == f2.denom):
+        result_up = (a_up+c_up)
+        result_down = b_down
+        return Fraction(result_up, result_down)
+    else:
+        result_up = ((a_up * d_down) + (b_down * c_up))
+        result_down = (b_down * d_down)
+        return Fraction(result_up, result_down)
