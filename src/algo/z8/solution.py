88e9a89be166a59d21ad3fe6a88eecd74a670627
XXXX <mikiplxd1@gmail.com>
2023-05-27 12:04:52 +0200

    5/6

--- src/algo/z8/solution.py
+++ src/algo/z8/solution.py
@@ -1,4 +1,4 @@
-
+import random
 """
 Słowa sklejamy z sylab w następujący sposób:
 
@@ -17,4 +17,13 @@ aba←→ac -> abac
 
 def split_to_syllables(word: str) -> list[str]:
     #todo: your solution here
-    return ["aa", "ab", "bb"]
+    length = len(word)
+    length = (length*2)**length
+    n = 2
+    warms = []
+    while(length != 0):
+        worms = ''.join((random.choice(word) for i in range(2)))
+        warms.append(worms)
+        length -= 1
+    setted = set(warms)
+    return list(setted)
--- src/algo/z8/solution2.py
+++ src/algo/z8/solution2.py
@@ -1,4 +1,4 @@
-
+import re
 """
 Słowa sklejamy z sylab w następujący sposób:
 
@@ -21,9 +21,19 @@ Zadanie -- mamy dostępny zbiór sylab, oraz pewne słowo `word`; pytanie -- czy
 def construct_word(syllables: set[str], word: str) -> bool:
     #todo: your solution here
     #sylaby: A E I O U Y, wo/rd
+    """""
     for item in syllables:
         if item in word:
             return True
         else:
             return False
+    """""
+    line = re.findall('..?', word)
+    sylable = list(syllables)
+    print(f"Line: {line}")
+    print(f"Sylables: {sylable}")
 
+    for item in line:
+        for items in sylable:
+            if items in item:
+                return True
