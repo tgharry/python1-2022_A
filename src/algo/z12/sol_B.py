511e5878d7dd5b22749cd48bc99bc425eec06537
tgharry50 <mikiplxd1@gmail.com>
2023-07-01 09:27:20 +0200

    Works i guess ?

--- src/algo/z12/sol_B.py
+++ src/algo/z12/sol_B.py
@@ -4,6 +4,19 @@
 def get_best_value_package(costs: list[int], values: list[int], max_total_cost: int) -> int:
     # znaleźć najlepszą możliwą wartość podzbioru itemów, ale takich by łączny "cost" tych itemów nie przekraczał
     # wartości max_total_cost
-    pass
+    num_items=len( costs )
+    dp=[0] * (max_total_cost + 1)
+
+    for i in range( num_items ) :
+        for j in range( max_total_cost, costs[i] - 1, -1 ) :
+            dp[j]=max( dp[j], values[i] + dp[j - costs[i]] )
+
+    return dp[max_total_cost]
+
+
+
+
+
+
 
 
