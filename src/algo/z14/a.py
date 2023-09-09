c2b48cc13bcaa1152e2507dcd28e9df2d1375658
tgharry50 <127186125+tgharry50@users.noreply.github.com>
2023-09-09 14:37:34 +0200

    a

--- src/algo/z14/a.py
+++ src/algo/z14/a.py
@@ -1,6 +1,7 @@
 def is_on_board(position: tuple[int, int], max_dim: tuple[int, int] = (8, 8)):
     x,y = position
     x_d, y_d = max_dim
+
     if x>=0 and y>=0 and x_d>=0 and x_d>=0:
         if x <x_d and y<y_d:
             return True
