73715ef30b197b5e7c286f2047ec3175fc42ec56
tgharry50 <127186125+tgharry50@users.noreply.github.com>
2023-09-16 14:00:27 +0200

    26

--- src/algo/z14/c.py
+++ src/algo/z14/c.py
@@ -2,6 +2,6 @@ def gen_new_positions(position: tuple[int, int], max_dim: tuple[int,int]) -> lis
     knight_moves = [(-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, -1), (2, 1)]
     makes_move = []
     for item in knight_moves:
-        makes_move.append((position[0] + i[0], position[1] + i[1]))
+        makes_move.append((position[0] + item[0], position[1] + item[1]))
     return makes_move
 
